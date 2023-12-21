30479a9bb3edad5cd7c4bd260d74797beaf57264
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-12-21 12:03:26 +0100

    add CSS font-face

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
+import 'fonts/Roboto-Bold.ttf'
+import 'fonts/Roboto-Medium.ttf'
+import 'fonts/Roboto-Regular.ttf'
+
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
