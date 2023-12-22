f6764b61bac67bb3e528ccca7097df9abea99df5
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-12-23 00:03:25 +0200

    Added a place in the App.jsx for the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
+// import { lazy, useEffect } from 'react';
 // import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+// const HomePage = lazy(() => import(''));
+// const SingUpPage = lazy(() => import(''));
 // const SignInPage = lazy(() => import(''));
+// const MainPage = lazy(() => import(''));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="signin"  />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/'> {/* SharedLayout */}
+          <Route index /> {/* MainPage */}
+          <Route path='signin' /> {/* SignInPage */}
+          <Route path='signup' /> {/* SignUpPage */}
+          <Route path='/homepage' /> {/* HomePage */}
+        </Route>
+      </Routes>
+    </>
+    
   );
 };
 
