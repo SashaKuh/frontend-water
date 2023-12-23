d2b31035f60b42726c0f276030c6cc82fce00df1
Vira <35749970+Vestanra@users.noreply.github.com>
2023-12-23 23:10:00 +0200

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,13 @@
 // import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { DailyNorma } from './DailyNorma/DailyNorma';
 
 // const SignInPage = lazy(() => import(''));
 
 export const App = () => {
-  return (<div><DailyNorma/>
+  return (
     <Routes>
       <Route path="signin"  />
-    </Routes></div>
+    </Routes>
   );
 };
 
