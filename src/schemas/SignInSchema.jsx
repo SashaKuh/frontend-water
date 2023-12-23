cd6677a0fb43990ecbb57439dbb30f3aa2742d26
FX-IT-Marina <121512261+FX-IT-Marina@users.noreply.github.com>
2023-12-23 11:41:08 +0100

    Update SignInSchema.jsx

---
+++ src/schemas/SignInSchema.jsx
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+export const signInSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/,
+      'Password must include letters, numbers, special characters'
+    )
+    .min(8, 'Too short password')
+    .max(64, 'Too long password')
+    .required('Password is required'),
+});
