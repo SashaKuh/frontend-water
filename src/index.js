ea329e980961cdc1442d124c7e295f7103cc6bd0
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-12-25 20:56:16 +0200

    coment

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { ThemeProvider } from 'styled-components';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react'
 
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyles';
 import './index.css';
-import { store, persistor } from './redux/store.js';
+// import { store, persistor } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}> 
+    {/* <Provider store={store}>
+      <PersistGate persistor={persistor}>  */}
         <GlobalStyle/>
         <BrowserRouter basename="/frontend-water">
           <App />
         </BrowserRouter>
-      </PersistGate>
-    </Provider>
+      {/* </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//     persistStore,
+//     persistReducer,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+// } from 'redux-persist';
 
-import sessionStorage from 'redux-persist/es/storage/session';
+// import sessionStorage from 'redux-persist/es/storage/session';
 
-// import authReducer
-// import modalReducer
-// import waterReducer
+// // import authReducer
+// // import modalReducer
+// // import waterReducer
 
-const persistConfig = {
-    key: 'root',
-    Storage: sessionStorage,
-    whitelist: ['token']
-};
+// const persistConfig = {
+//     key: 'root',
+//     Storage: sessionStorage,
+//     whitelist: ['token']
+// };
 
-export const store = configureStore({
-    reducer: {
-        auth: persistReducer(persistConfig, {/*authReducer*/ }),
-        modals: {/*modalReducer*/ },
-        water: {/*waterReducer*/ }
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-    evTools: process.env.NODE_ENV === 'development'
-});
+// export const store = configureStore({
+//     reducer: {
+//         auth: persistReducer(persistConfig, {/*authReducer*/ }),
+//         modals: {/*modalReducer*/ },
+//         water: {/*waterReducer*/ }
+//     },
+//     middleware: getDefaultMiddleware =>
+//         getDefaultMiddleware({
+//             serializableCheck: {
+//                 ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//             },
+//         }),
+//     evTools: process.env.NODE_ENV === 'development'
+// });
 
-export const persistor = persistStore(store)
+// export const persistor = persistStore(store)
