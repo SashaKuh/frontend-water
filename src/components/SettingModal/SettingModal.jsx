9e36ed75dd8239464883615079b4d32d01d78886
Vira <35749970+Vestanra@users.noreply.github.com>
2023-12-26 23:22:07 +0200

    save

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,11 +1,59 @@
-import { Formik } from "formik";
-import { StModalBackdrope, StModalWrap, StyledSettingModal, Title } from "./SettingModal.styled"
+import { useFormik } from "formik";
+import sprite from "../../images/SVG/symbol-defs.svg";
+import {
+    StModalBackdrope, StModalWrap, StyledSettingModal, Title, BtnSvg, SvgClose, WrapHeader, ImgWrapper, ImgAvatar,
+    AvatarWrap, UploadLabel, FileInput, UploadButton, StyledRadioGroup, StyledRadioInput, StyledRadioCircle, StyledRadioLabel,
+    StyledRadioText, CircleColor, Button, SvgUpload, WrapInfo, Input, EmailText, PasswordText, PasswordTitle, InputPassword, SvgEye, ButtonEye, InputPasswordWrap,  
+} from "./SettingModal.styled"
+import { useState } from "react";
+import { number } from "yup";
 
 export const SettingModal = ({ modalIsOpen, closeModal }) => {
+    const avatarImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRypDF0qZ728h4xrKppmUyL6jzA4DxVjHF-g&usqp=CAU";
+    const gender = "girl";
+    const name = "Vira";
+    const email = "vira@ukr.net";
+
+    const [showPassword, setShowPassword] = useState(false);
+    const formik = useFormik({
+        initialValues: {
+            gender,
+            name: null,
+            email: null,
+            oldPassword: null,
+            newPassword: null,
+            confirmPassword: null,
+            avatarUrl: avatarImg,
+        },
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    });
+
+    const handleFileChange = (evt) => {
+        formik.setFieldValue("avatarUrl", URL.createObjectURL(evt.currentTarget.files[0]));
+    };
+
+    const handleGenderChange = (evt) => {
+        formik.setFieldValue("gender", evt.target.value);
+    };
+
+    const handleInputChange = (evt) => {
+        formik.handleChange(evt); 
+    };
+    
+    const handleCloseModal = () => {
+        closeModal();
+        formik.resetForm()
+    };
+
+    const handleTogglePassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
+
     const onBackdropeClick = (evt) => {
         if (evt.target === evt.currentTarget) {
-            closeModal();
-            // resetForm();
+            handleCloseModal()
         }
     }
     return (
@@ -14,24 +62,105 @@ export const SettingModal = ({ modalIsOpen, closeModal }) => {
             onRequestClose={closeModal}>
             <StModalBackdrope onClick={onBackdropeClick}>
                 <StModalWrap>
-                    <Title>Setting</Title>
-                    <Formik
-                        initialValues={{
-                            photo: "",
-                            gender: "",
-                            name: "",
-                            email: "",
-                            oldPassword: "",
-                            newPassword: "",
-                            confirmPassword:"",
-                        }}
-                        validateOnBlur
-                        onSubmit={(values) => {
-                            console.log(values)
-                        }}
-                    >
-
-                    </Formik>
+                    <WrapHeader>
+                        <Title>Setting</Title>
+                        <BtnSvg type="button" onClick={handleCloseModal}>
+                            <SvgClose >
+                                <use xlinkHref={`${sprite}#icon-plus-small`} />
+                            </SvgClose>
+                        </BtnSvg>
+                    </WrapHeader>
+                    <form onSubmit={formik.handleSubmit}>
+                        <p>Your photo</p>
+                        <AvatarWrap>
+                            <ImgWrapper>
+                                <ImgAvatar src={formik.values.avatarUrl} alt="User Avatar" />
+                            </ImgWrapper>
+                            <UploadLabel>
+                                <FileInput name="avatarUrl" type="file" accept="image/*" onChange={handleFileChange} />
+                                <UploadButton>
+                                    <SvgUpload width="16" height="16">
+                                        <use xlinkHref={`${sprite}#icon-arrow-up`} />
+                                    </SvgUpload>
+                                    <p>Upload a photo</p>
+                                </UploadButton>
+                            </UploadLabel>
+                        </AvatarWrap>
+                        <WrapInfo>
+                            <div>
+                                <p>Your gender identity</p>
+                                <StyledRadioGroup role="group" aria-labelledby="my-radio-group">
+                                    <StyledRadioLabel>
+                                        <StyledRadioInput type="radio" name="gender" value="girl" onChange={handleGenderChange} />
+                                        <StyledRadioCircle><CircleColor checked={formik.values.gender === 'girl'}></CircleColor></StyledRadioCircle>
+                                        <StyledRadioText>Girl</StyledRadioText>
+                                    </StyledRadioLabel>
+                                    <StyledRadioLabel>
+                                        <StyledRadioInput type="radio" name="gender" value="man" onChange={handleGenderChange} />
+                                        <StyledRadioCircle><CircleColor checked={formik.values.gender === 'man'}></CircleColor></StyledRadioCircle>
+                                        <StyledRadioText>Man</StyledRadioText>
+                                    </StyledRadioLabel>
+                                </StyledRadioGroup>
+                                <label>
+                                    <p>Your name</p>
+                                    <Input name="name" type="text" placeholder={name} onChange={handleInputChange}/>
+                                </label>
+                                <label>
+                                    <EmailText>E-mail</EmailText>
+                                    <Input name="email" type="email" placeholder={email} onChange={handleInputChange}/>
+                                </label>
+                            </div>
+                            <div>
+                                <PasswordTitle>Password</PasswordTitle>
+                                <label>
+                                    <PasswordText>Outdated password:</PasswordText>
+                                    <InputPasswordWrap>
+                                        <InputPassword name="oldPassword" onChange={handleInputChange} type={showPassword ? "text" : "password"} placeholder="Password" />
+                                        <ButtonEye onClick={handleTogglePassword}>
+                                            {showPassword ?
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye`} />
+                                                </SvgEye> :
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye-slash`} />
+                                                </SvgEye>}
+                                        </ButtonEye>
+                                    </InputPasswordWrap>
+                                </label>
+                                <label>
+                                    <PasswordText>New Password:</PasswordText>
+                                    <InputPasswordWrap>
+                                        <InputPassword name="newPassword" onChange={handleInputChange} type={showPassword ? "text" : "password"} placeholder="Password" />
+                                        <ButtonEye onClick={handleTogglePassword}>
+                                            {showPassword ?
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye`} />
+                                                </SvgEye> :
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye-slash`} />
+                                                </SvgEye>}
+                                        </ButtonEye>
+                                    </InputPasswordWrap>
+                                </label>
+                                <label>
+                                    <PasswordText>Repeat new password:</PasswordText>
+                                    <InputPasswordWrap>
+                                        <InputPassword name="confirmPassword" onChange={handleInputChange} type={showPassword ? "text" : "password"} placeholder="Password" />
+                                        <ButtonEye onClick={handleTogglePassword}>
+                                            {showPassword ?
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye`} />
+                                                </SvgEye> :
+                                                <SvgEye width="16" height="16">
+                                                    <use xlinkHref={`${sprite}#icon-eye-slash`} />
+                                                </SvgEye>}
+                                        </ButtonEye>
+                                    </InputPasswordWrap>
+                                </label>
+                            </div>
+                        </WrapInfo>
+                        <Button type="submit">Save</Button>
+                    </form>
                 </StModalWrap>
             </StModalBackdrope>
         </StyledSettingModal>
--- src/components/SettingModal/SettingModal.styled.js
+++ src/components/SettingModal/SettingModal.styled.js
@@ -21,13 +21,12 @@ export const StyledSettingModal = styled(ReactModal)`
 
 export const StModalWrap = styled.div`
 background-color: var(--primaryWhite);
-font-family: 'Roboto-regular';
 color: var(--primaryBlack);
 padding: 32px 12px;
 border-radius: 10px;
-position: relative;
-font-size: 16px;
-line-height: 1.2;
+font-size: 18px;
+line-height: 1.1;
+font-family: 'Roboto-Medium';
 box-sizing: border-box;
 
 @media (min-width: 1440px) {
@@ -45,7 +44,238 @@ box-sizing: border-box;
 `;
 
 export const Title = styled.h1`
-font-family: 'Roboto-Medium';
+font-family: 'Roboto-regular';
 font-size: 26px;
-line-height: 1.2;
+line-height: 1.25;
+margin-bottom: 24px;
+`;
+
+export const BtnSvg = styled.button`
+padding: 0;
+width: 36px;
+height: 36px;
+border: none;
+background-color: var(--primaryWhite);
+
+@media (min-width: 768px) and (max-width: 1439px){
+    top: 36px; 
+    right: 24px;
+  }
+`;
+
+export const SvgClose = styled.svg`
+fill: var(--primaryBlue);
+transform: rotate(45deg); 
+width: 36px;
+height: 36px;
+`;
+
+export const WrapHeader = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+export const ImgWrapper = styled.div`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+export const ImgAvatar = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; 
+`;
+
+export const AvatarWrap = styled.div`
+display: flex;
+margin-top: 8px;
+margin-bottom: 24px;
+gap: 8px;
+`;
+
+export const WrapInfo = styled.div`
+@media (min-width: 1440px) {
+    display: flex;
+    gap: 24px;
+  }
+`;
+
+export const UploadLabel = styled.label`
+display: flex;
+position: relative;
+`;
+
+export const FileInput = styled.input`
+  display: none;
+`;
+
+export const UploadButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px; 
+  cursor: pointer;
+  color: var(--primaryBlue);
+  font-size: 14px;
+  font-family: 'Roboto-Medium';
+  line-height: 1.3;
+`;
+
+export const SvgUpload = styled.svg`
+fill: var(--primaryBlue);
+`;
+
+export const StyledRadioGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 12px;
+  margin-bottom: 24px;
+  @media (min-width: 1440px) {
+    margin-bottom: 52px;
+  }
+`;
+
+export const StyledRadioLabel = styled.label`
+  position: relative;
+  cursor: pointer;
+  margin-right: 24px;
+`;
+
+export const StyledRadioInput = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+`;
+
+export const StyledRadioCircle = styled.span`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  transform: translate(-50%, -50%);
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+  border: 1px solid #007BFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CircleColor = styled.div`
+  background-color: ${(props) => (props.checked ? '#007BFF' : '#fff')};
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+`;
+
+export const StyledRadioText = styled.span`
+  margin-left: 22px;
+  font-family: 'Roboto-regular';
+  font-size: 16px;
+  line-height: 1.25;
+`;
+
+export const Button = styled.button`
+margin-top: 24px;
+background-color: var(--primaryBlue);
+border: none;
+width: 100%;
+height: 36px;
+color: var(--primaryWhite);
+font-size: 16px;
+font-family: 'Roboto-Medium';
+line-height: 1.3;
+border-radius: 10px;
+box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+
+@media (min-width: 768px) {
+    display: block;
+    font-size: 18px;
+    width: 160px;
+    height: 44px;
+    margin-left: auto;
+  }
+`;
+
+export const Input = styled.input`
+  margin-top: 8px;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid var(--secondaryDarkGrey);
+  border-radius: 8px;
+  padding: 12px 10px;
+  outline: none;
+  color: #407BFF;
+  font-size: 16px;
+  &::placeholder {
+    color: var(--secondaryBlue);
+  }
+
+  @media (min-width: 768px) {
+    width: 392px;
+  }
+`;
+
+export const EmailText = styled.p`
+margin-top: 24px;
+`;
+
+export const PasswordTitle = styled.p`
+margin-top: 24px;
+@media (min-width: 1440px) {
+  margin-top: 0;
+  }
+`;
+
+export const PasswordText = styled.p`
+margin-top: 12px;
+font-family: 'Roboto-regular';
+font-size: 16px;
+line-height: 1.25;
+`;
+
+export const InputPassword = styled.input`
+  margin-top: 8px;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid var(--secondaryDarkGrey);
+  border-radius: 8px;
+  padding: 12px 10px;
+  outline: none;
+  color: #407BFF;
+  font-size: 16px;
+  &::placeholder {
+    color: var(--secondaryBlue);
+  }
+
+  @media (min-width: 768px) {
+    width: 392px;
+  }
+  `;
+
+export const InputPasswordWrap = styled.div`
+position: relative;
+display: flex;
+align-items: center;
+width: 100%;
+@media (min-width: 768px) {
+    width: 392px;
+  }
+`;
+
+export const SvgEye = styled.svg`
+width: 18px;
+height: 18px;
+fill: var(--primaryBlue);
+`;
+
+export const ButtonEye = styled.button`
+position: absolute;
+width: 16px;
+height: 16px;
+background-color: var(--primaryWhite); 
+border: none;
+top: 22px;
+right: 22px;
 `;
