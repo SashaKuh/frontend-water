93b3c52a54aa909ad8de636567cfdeb8a9ceafe7
MemologLeza <lizabakay1@gmail.com>
2023-12-26 21:11:10 +0000

    fix structure of HomePage Components
    Destructuring the file Components/HomePage into separate component files: DailyNorma, ImageOfBottle, and WaterRatioPanel.

---
+++ src/components/HomePage/WaterRatioPanel.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import iconSprite from '../../images/SVG/symbol-defs.svg';
+import {
+  RangeAndAddWater,
+  RangeDiv,
+  RangeTitle,
+  StyledRangeInput,
+  PercentageDiv,
+  PercentageOfRange,
+  BoldPercentageOfRange,
+  ButtonAddWater,
+  SvgButton,
+} from './HomePage.styled';
+export const WaterRatioPanel = () => {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleChange = event => {
+    const value = event.target.value;
+    setSliderValue(value);
+  };
+
+  const progress = (sliderValue / 100) * 100;
+
+  const sliderStyle = {
+    background: `linear-gradient(to right, #9EBBFF ${progress}%, #D7E3FF ${progress}%)`,
+  };
+  return (
+    <RangeAndAddWater>
+      <RangeDiv>
+        <RangeTitle>Today</RangeTitle>
+        <StyledRangeInput
+          type="range"
+          min="0"
+          max="100"
+          value={sliderValue}
+          onChange={handleChange}
+          style={sliderStyle}
+        ></StyledRangeInput>
+        <PercentageDiv>
+          <PercentageOfRange>0%</PercentageOfRange>
+          <PercentageOfRange>
+            <BoldPercentageOfRange>50%</BoldPercentageOfRange>
+          </PercentageOfRange>
+          <PercentageOfRange>100%</PercentageOfRange>
+        </PercentageDiv>
+      </RangeDiv>
+      <ButtonAddWater>
+        <SvgButton>
+          <use href={iconSprite + '#icon-plus-circle'} />
+        </SvgButton>
+        Add Water
+      </ButtonAddWater>
+    </RangeAndAddWater>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,13 @@
-import HomePage from '../../components/HomePage/HomePage';
-
-export default function SignIn() {
-  return <HomePage />;
-}
+import { DailyNorma } from 'components/DailyNorma/DailyNorma';
+import { ImageOfBottle } from 'components/HomePage/ImageOfBottle';
+import { WaterRatioPanel } from 'components/HomePage/WaterRatioPanel';
+const HomePage = () => {
+  return (
+    <div className="container">
+      <DailyNorma />
+      <ImageOfBottle />
+      <WaterRatioPanel />
+    </div>
+  );
+};
+export default HomePage;
