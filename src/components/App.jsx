4d714b80ff27946b0129361a602208c4c684d2f3
MemologLeza <lizabakay1@gmail.com>
2023-12-26 02:18:57 +0000

    Create adaptive HomePage for auth user firs part(
    Adaptive HomePage: the first part features "Daily Norma of Water," a bottle of water image, and a custom range input with a button labeled "Add Water" components, without a connection to the modal "Add Water" and the change from "Daily Norma.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// const HomePage = lazy(() => import(''));
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const SignUpPage = lazy(() => import('../pages/SignUpPage/SignUpPage'));
 const SignInPage = lazy(() => import('../pages/SignInPage/SignInPage'));
 // const MainPage = lazy(() => import(''));
@@ -11,11 +11,12 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/">{/* SharedLayout */}
+        <Route path="/">
+          {/* SharedLayout */}
           <Route index /> {/* MainPage */}
-          <Route path="signin" element={<SignInPage /> } /> 
+          <Route path="signin" element={<SignInPage />} />
           <Route path="signup" element={<SignUpPage />} />
-          <Route path="/homepage" /> {/* HomePage */}
+          <Route path="/homepage" element={<HomePage />} />
         </Route>
       </Routes>
     </>
