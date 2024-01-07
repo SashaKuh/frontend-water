6d3659d249abe741b213b5dfc8a16ddeaa33ac8f
Vira <35749970+Vestanra@users.noreply.github.com>
2024-01-07 18:41:14 +0200

    save

--- src/components/EditModal/EditModal.jsx
+++ src/components/EditModal/EditModal.jsx
@@ -28,6 +28,7 @@ import {
     WaterText,
     TimeText,
 } from '../TodayItem/TodayItem.styled';
+import { disabledTime } from 'helpers/disabledTime';
 
 const glassIcon = `${sprite}#cup`;
 
@@ -77,21 +78,6 @@ export const EditModal = ({ modalIsOpen, closeModal, date, id }) => {
         }
         await dispatch(getMonthWaterOperation(new Date()));
         handleCloseModal();
-    };
-
-    const disabledTime = (now) => {
-        const currentHour = now.hour();
-        const disabledHours = () => {
-            return Array.from({ length: 24 }, (_, index) => (index > currentHour ? index : -1)).filter((hour) => hour !== -1);
-        };
-
-        const disabledMinutes = (selectedHour) => {
-            return selectedHour === currentHour
-                ? Array.from({ length: 60 }, (_, index) => (index > now.minute() ? index : -1)).filter((minute) => minute !== -1)
-                : [];
-        };
-
-        return { disabledHours, disabledMinutes };
     };
 
     return (
