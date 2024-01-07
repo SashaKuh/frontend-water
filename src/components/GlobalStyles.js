5f23bfbcfbc48fddfe2bd5f09d3660aee36b8c12
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2024-01-07 17:15:42 +0100

    return static bg wellcome page

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -67,13 +67,23 @@ body {
 }
 
 
-.response-background {
-
-
-
+.static-background {
   width: 100%;
   margin-right: auto;
   margin-left: auto;
+  @media screen and (min-width: 320px) {
+        max-width: 320px;
+        }
+  
+  @media screen and (min-width: 768px) {
+        max-width: 768px;
+       
+        }
+
+    @media screen and (min-width: 1440px) {
+        max-width: 1440px;
+        
+    }
 }
 
 code {
