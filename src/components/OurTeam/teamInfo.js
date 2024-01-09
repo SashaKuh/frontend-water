03266b871b17d3cffb8d5adc503f041f29842e0f
Vira <35749970+Vestanra@users.noreply.github.com>
2024-01-09 22:10:07 +0200

    add foto

--- src/components/OurTeam/teamInfo.js
+++ src/components/OurTeam/teamInfo.js
@@ -2,20 +2,25 @@ import vira from "../../images/team/vira.jpg"
 import Kuharchuk from "../../images/team/im.png"
 import Rostyslav from "../../images/team/rost.jpg"
 import Taran from "../../images/team/liza.jpg"
+import Marina from "../../images/team/marina.jpg"
+import Bakai from "../../images/team/Bakai.png"
+import Yaroslav from "../../images/team/Yaroslav.jpg"
+import bohdan from "../../images/team/bohdan.jpg"
+import Dmytro from "../../images/team/Dmytro.jpg"
 
 export const team = [
     {
         name: "Olexandr Kuharchuk",
-        linkedin: "",
-        github: "",
+        linkedin: "https://www.linkedin.com/in/olexakuhar/",
+        github: "https://github.com/SashaKuh",
         image: Kuharchuk,
         id: 1,
     },
     {
         name: "Bohdan Horetskyi",
-        linkedin: "",
-        github: "",
-        image: "",
+        linkedin: "https://www.linkedin.com/in/bohdan-horetskyi/",
+        github: "https://github.com/bhoretskyi",
+        image: bohdan,
         id: 2,
     },
     {
@@ -27,44 +32,44 @@ export const team = [
     },
     {
         name: "Rostyslav Stetsyk",
-        linkedin: "",
-        github: "",
+        linkedin: "https://www.linkedin.com/in/rostyslav-stetsyk/",
+        github: "https://github.com/Rostyslav-Stetsyk",
         image: Rostyslav,
         id: 4,
     },
     {
         name: "Yelyzaveta Bakai",
-        linkedin: "",
-        github: "",
-        image: "",
+        linkedin: "https://www.linkedin.com/in/yelyzaveta-bakai/",
+        github: "https://github.com/MemologLeza",
+        image: Bakai,
         id: 5,
     },
     {
         name: "Yelyzaveta Taran",
-        linkedin: "",
-        github: "",
+        linkedin: "https://www.linkedin.com/in/yelyzaveta-taran/",
+        github: "https://github.com/taranlizz",
         image: Taran,
         id: 6,
     },
     {
         name: "Yaroslav Kryvusha",
-        linkedin: "",
-        github: "",
-        image: "",
+        linkedin: "https://www.linkedin.com/in/yaroslav-kryvusha/",
+        github: "https://github.com/yaroslawv-nexus",
+        image: Yaroslav,
         id: 7,
     },
     {
         name: "Maryna Kalataliuk",
-        linkedin: "",
-        github: "",
-        image: "",
+        linkedin: "https://www.linkedin.com/in/maryna-kalataliuk/",
+        github: "https://github.com/FX-IT-Marina",
+        image: Marina,
         id: 8,
     },
     {
-        name: "Dmytro",
-        linkedin: "",
-        github: "",
-        image: "",
+        name: "Dmytro Baranovskyi",
+        linkedin: "https://www.linkedin.com/in/dmytro-baranovskyi-282944105/",
+        github: "https://github.com/dymitrbaranowski",
+        image: Dmytro,
         id: 9,
     },
 ]
